748fd1cf26c10a51aa0f4d02fcd672090eba6e4d
ait <ait@ex.in>
2020-03-10 04:25:40 +0900

    date select

--- pages/setting.vue
+++ pages/setting.vue
@@ -7,57 +7,52 @@
 
         <v-row no-gutters>
           <v-col lg="6">
-            <h2>条件入力</h2>
+            <h2 class="mt-4 ml-3">条件入力</h2>
             <div class="d-inline-flex">
               <v-col cols="4">
-                <v-select :items="teams" label="Standard"></v-select>
-              </v-col>
-              <v-col cols="6">
-                <v-select :items="teams" label="Standard"></v-select>
+                <v-select :items="teams" label="病棟"></v-select>
               </v-col>
               <v-col cols="4">
-                <v-select :items="teams" label="Standard"></v-select>
+                <v-select :items="year" label="年"></v-select>
+              </v-col>
+              <v-col cols="4">
+                <v-select :items="month" label="月"></v-select>
               </v-col>
             </div>
           </v-col>
         </v-row>
 
-        <v-tabs color="cyan" dark slider-color="yellow">
-          <v-tab ripple>
-            病院条件
-          </v-tab>
-          <v-tab ripple>
-            個人条件
-          </v-tab>
+        <v-tabs color="teal" slider-color="teal">
+          <v-tab ripple>病院条件</v-tab>
+          <v-tab ripple>個人条件</v-tab>
 
           <!-- tab1 -->
           <v-tab-item>
             <v-row>
               <!-- Layout-Left  -->
-              <v-col cols="3" style="">
+              <v-col cols="6" style="">
                 <!-- Basic Conditions -->
                 <v-card class="pa-1" outlined tile>
                   <v-col cols="12">
                     <v-card class="pa-1" outlined tile>
                       <v-col cols="12">
-                        <h3 class="pb-2">Basic Conditions</h3>
-                        <v-switch v-model="switch1" :label="`Lock: ${switch1.toString()}`"></v-switch>
-                        <div class="d-flex flex-column">
-                          <div class="form_title">MemberID：</div>
+                        <h3 class="pb-2">基本条件入力</h3>
+                        <div class="mt-4 d-flex flex-column">
+                          <div class="form_title">所定労働時間</div>
                           <div class="form_field">
-                            <v-text-field label="" single-line></v-text-field>
+                            <v-text-field label="時間" single-line></v-text-field>
                           </div>
                         </div>
                         <div class="d-flex flex-column">
-                          <div class="form_title">Boss Name：</div>
+                          <div class="form_title">総夜勤時間：</div>
                           <div class="form_field">
-                            <v-text-field label="" single-line></v-text-field>
+                            <v-text-field label="時間" single-line></v-text-field>
                           </div>
                         </div>
                         <div class="d-flex flex-column">
-                          <div class="form_title">Team：</div>
+                          <div class="form_title">公休：</div>
                           <div class="form_field">
-                            <v-text-field label="" single-line></v-text-field>
+                            <v-text-field label="日" single-line></v-text-field>
                           </div>
                         </div>
                       </v-col>
@@ -66,109 +61,46 @@
                 </v-card>
                 <!-- /Basic Conditions -->
 
-                <!-- Month Conditions -->
-                <v-card class="pa-1" outlined tile>
-                  <v-col cols="12">
-                    <v-card class="pa-1" outlined tile>
-                      <v-col cols="12">
-                        <h3 class="pb-4">Month Conditions </h3>
-                        <!-- Date Picker -->
-                        <div class="d-flex flex-column">
-                          <div class="form_title">Pick Month：</div>
-                          <div class="form_field">
-                            <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y max-width="290px" min-width="290px">
-                              <template v-slot:activator="{ on }">
-                                <v-text-field v-model="date" label="" prepend-icon="event" readonly v-on="on"></v-text-field>
-                              </template>
-                              <v-date-picker v-model="date" type="month" :landscape="landscape" :reactive="reactive" locale="jp-ja" :day-format="date => new Date(date).getDate()" no-title scrollable>
-                                <v-spacer></v-spacer>
-                                <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-                                <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-                              </v-date-picker>
-                            </v-menu>
-                          </div>
-                        </div>
-                        <!-- / Date Picker -->
 
-                        <div class="d-flex flex-column">
-                          <div class="form_title">WorkTime：</div>
-                          <div class="form_field">
-                            <v-text-field label="" single-line></v-text-field>
-                          </div>
-                        </div>
-                        <div class="d-flex flex-column">
-                          <div class="form_title">Holiday：</div>
-                          <div class="form_field">
-                            <v-text-field label="" single-line></v-text-field>
-                          </div>
-                        </div>
 
-                        <div class="d-flex flex-column">
-                          <div class="form_title">22:00：</div>
-                          <div class="form_field">
-                            <v-text-field label="" single-line></v-text-field>
-                          </div>
-                        </div>
-                      </v-col>
-                    </v-card>
+                <!-- ToDo List -->
+                <v-card class="totoList pa-1 mt-4" outlined tile>
+                  <v-col cols="12" class="">
+                    <div>
+                      <h4 class="totoList pt-1 mb-2">Work Styles</h4>
+                      <!-- v-on:keyup.enter="addTodo(newItemTitle) -->
+                      <input type="text" placeholder="input Style" v-model="newItemTitle">
+
+                      <select v-model="newItemSelect">
+                        <option value="08:00">08:00</option>
+                        <option value="22:00">22:00</option>
+                        <option value="12:00">12:00</option>
+                      </select>
+                      〜
+                      <select v-model="newItemJob">
+                        <option value="19:00">19:00</option>
+                        <option value="12:00">12:00</option>
+                        <option value="6:00">6:00</option>
+                      </select>
+
+                      <button type="button" name="button" v-on:click="addTodo(newItemTitle, newItemSelect, newItemJob)">追加</button>
+                    </div>
+
+                    <ul>
+                      <li v-for="item in items" :key="item.index">
+                        <p>
+                          <label v-bind:class="{ done: item.isChecked }">
+                            {{ item.title }} ｜ {{ item.selItem }} 〜 {{ item.itemJob }} <input type="checkbox" v-model="item.isChecked">
+                          </label>
+                        </p>
+                      </li>
+                    </ul>
+
+                    <button v-on:click="deleteTodo()">Delete</button>
+
                   </v-col>
                 </v-card>
-                <!-- /Month Conditions -->
-
-                <!-- Week Conditions -->
-                <v-card class="pa-1" outlined tile>
-                  <v-col cols="12">
-                    <v-card class="pa-1" outlined tile>
-                      <v-col cols="12">
-                        <h3 class="pb-3">Week Conditions</h3>
-                        <!-- <h4 class="pb-1">Weekdays</h4> -->
-
-                        <div class="d-flex flex-column">
-                          <h5 class="grey lighten-2 pa-1 mb-2 mt-1">Hi Class：</h5>
-                          <div class="form_title"></div>
-                          <div class="form_field">
-                            <v-text-field label="Weekdays" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Saturday" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Holiday" value="0" type="number" suffix=""></v-text-field>
-                          </div>
-                        </div>
-
-                        <div class="d-flex flex-column">
-                          <h5 class="grey lighten-2 pa-1 mb-2 mt-1">Mid Class：</h5>
-                          <div class="form_title"></div>
-                          <div class="form_field">
-                            <v-text-field label="Weekdays" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Saturday" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Holiday" value="0" type="number" suffix=""></v-text-field>
-                          </div>
-                        </div>
-
-                        <div class="d-flex flex-column">
-                          <h5 class="grey lighten-2 pa-1 mb-2 mt-1">Low Class：</h5>
-                          <div class="form_title"></div>
-                          <div class="form_field">
-                            <v-text-field label="Weekdays" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Saturday" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Holiday" value="0" type="number" suffix=""></v-text-field>
-                          </div>
-                        </div>
-
-                        <div class="d-flex flex-column">
-                          <h5 class="grey lighten-2 pa-1 mb-2 mt-1">Sub Class：</h5>
-                          <div class="form_title"></div>
-                          <div class="form_field">
-                            <v-text-field label="Weekdays" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Saturday" value="0" type="number" suffix=""></v-text-field>
-                            <v-text-field label="Holiday" value="0" type="number" suffix=""></v-text-field>
-                          </div>
-                        </div>
-
-                      </v-col>
-
-                    </v-card>
-                  </v-col>
-                </v-card>
-                <!-- /Week Conditions -->
+                <!-- /ToDo List -->
               </v-col>
               <!-- /Layout-Left  -->
 
@@ -177,7 +109,6 @@
 
                 <v-card class="pa-1" outlined tile>
                   <v-col cols="12">
-
 
                     <!-- Dialog -->
                     <v-card class="mb-4">
@@ -297,83 +228,14 @@
                         <li>MemberName/ Skill / Hope Day Job Night</li>
                         <li>MemberName/ Skill / Hope Day Job Night</li>
                         <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
-                        <li>MemberName/ Skill / Hope Day Job Night</li>
                       </ul>
                     </v-card>
-
-                    <!-- ToDo List -->
-                    <v-card class="totoList pa-1 mt-4" outlined tile>
-                      <v-col cols="12" class="">
-                        <div>
-                          <h4 class="totoList pt-1 mb-2">Work Styles</h4>
-                          <!-- v-on:keyup.enter="addTodo(newItemTitle) -->
-                          <input type="text" placeholder="input Style" v-model="newItemTitle">
-
-                          <select v-model="newItemSelect">
-                            <option value="08:00">08:00</option>
-                            <option value="22:00">22:00</option>
-                            <option value="12:00">12:00</option>
-                          </select>
-                          〜
-                          <select v-model="newItemJob">
-                            <option value="19:00">19:00</option>
-                            <option value="12:00">12:00</option>
-                            <option value="6:00">6:00</option>
-                          </select>
-
-                          <button type="button" name="button" v-on:click="addTodo(newItemTitle, newItemSelect, newItemJob)">追加</button>
-                        </div>
-
-                        <ul>
-                          <li v-for="item in items" :key="item.index">
-                            <p>
-                              <label v-bind:class="{ done: item.isChecked }">
-                                {{ item.title }} ｜ {{ item.selItem }} 〜 {{ item.itemJob }} <input type="checkbox" v-model="item.isChecked">
-                              </label>
-                            </p>
-                          </li>
-                        </ul>
-
-                        <button v-on:click="deleteTodo()">Delete</button>
-
-                      </v-col>
-                    </v-card>
-                    <!-- /ToDo List -->
 
                   </v-col>
                 </v-card>
               </v-col>
               <!-- /Main -->
 
-              <v-col cols="3">
-                <v-card class="pt-2" outlined style="" tile>
-                  <v-col cols="12">
-                    <h4 class="totoList pt-1 mb-2">Members</h4>
-                    <ul class="menberLists">
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                      <li draggable>MemberName/ vacation days / OverTime</li>
-                    </ul>
-                  </v-col>
-                </v-card>
-              </v-col>
 
             </v-row>
 
@@ -398,43 +260,30 @@
 <script>
 export default {
   data: () => ({
-    switch1: true,
     date: new Date().toISOString().substr(0, 7),
     menu: false,
     modal: false,
     dialog: false,
-    teams: ['2A', '2B'],
+    // getDay
+    year: [2018, 2019, 2020, 2021, 2022],
+    month: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    day: 1,
+    days_max: '',
+    teams: ['1A', '1B', '1C', '2A', '2B', '2C'],
     items: [{
-        title: 'DayWork',
-        isChecked: false,
-        selItem: "08:00",
-        itemJob: "19:00"
-      },
-      // {
-      //   title: 'nameB',
-      //   isChecked: true,
-      //   selItem: "22:00",
-      //   itemJob: "12:00"
-      // },
-      // {
-      //   title: 'nameC',
-      //   isChecked: false,
-      //   selItem: "12:00",
-      //   itemJob: "6:00"
-      // },
-      // {
-      //   title: 'nameD',
-      //   isChecked: false,
-      //   selItem: "08:00",
-      //   itemJob: "19:00"
-      // },
-    ],
+      title: 'DayWork',
+      isChecked: false,
+      selItem: "08:00",
+      itemJob: "19:00"
+    }, ],
     newItemTitle: 'DayWork',
     newItemSelect: '08:00',
     newItemJob: '19:00',
-
     active: false,
   }),
+  created: function() {
+    this.get_days();
+  },
   methods: { //methodsオプションをまるっと追加
     addTodo: function(newItemTitle, newItemSelect, newItemJob) {
       if (this.newItemTitle.length < 1) {
@@ -456,11 +305,17 @@ export default {
         return item.isChecked === false //チェックが付いていないものはスルーする
       });
     },
+    // 日の最大数を取得
+    get_days: function() {
+      this.days_max = new Date(this.year, this.month, 0).getDate();
+    },
   },
 }
 </script>
 
 <style lang="sass" scoped>
+h1,h2,h3,h4
+  font-weight: 400
 .wrap
   max-width: 1600px
   min-width: 80vw
@@ -505,6 +360,5 @@ export default {
     padding: 4px
   select
    height: 32px
-
 
 </style>
